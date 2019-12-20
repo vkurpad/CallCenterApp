8cc58461599b52df973d22d2392b2c5b05128c32
Vinod Kurpad <vikurpad@microsoft.com>
2019-12-20 10:13:27 -0800

    Version1.0 of the CallCenter app

---
+++ CallCenterApp/Controllers/HomeController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CallCenterApp.Models;
+using Microsoft.Azure.Search;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.Search.Models;
+
+namespace CallCenterApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static SearchServiceClient _serviceClient;
+        private static ISearchIndexClient _indexClient;
+        private IConfiguration configuration;
+
+        public HomeController(IConfiguration iconfig)
+        {
+            configuration = iconfig;
+            
+        }
+        private void InitSearch()
+        {
+
+            string searchServiceName = configuration.GetValue<string>("prefixName") + "search"; 
+            string queryApiKey = configuration.GetValue<string>("searchMgmtKey"); 
+
+            // Create a service and index client.
+            _serviceClient = new SearchServiceClient(searchServiceName, new SearchCredentials(queryApiKey));
+            _indexClient = _serviceClient.Indexes.GetClient("callcenter-index");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Index(SearchData model)
+        {
+            try
+            {
+                // Ensure the search string is valid.
+                if (model.searchText == null)
+                {
+                    model.searchText = "";
+                }
+
+                // Make the Azure Search call.
+                await RunQueryAsync(model);
+            }
+
+            catch
+            {
+                return View("Error", new ErrorViewModel { RequestId = "1" });
+            }
+            return View(model);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private async Task<ActionResult> RunQueryAsync(SearchData model)
+        {
+            InitSearch();
+
+            var parameters = new SearchParameters
+            {
+                // Enter Hotel property names into this list so only these values will be returned.
+                // If Select is empty, all values will be returned, which can be inefficient.
+                Select = new[] { "audio_file", "summary", "conversation" }
+            };
+            try
+            {
+                // For efficiency, the search call should be asynchronous, so use SearchAsync rather than Search.
+                model.resultList = await _indexClient.Documents.SearchAsync<CallCenterLog>(model.searchText, parameters);
+            }
+            catch(Exception  ex)
+            {
+                throw ex;
+            }
+
+            // Display the results.
+            return View("Index", model);
+        }
+    }
+}
---
+++ CallCenterApp/Models/CallCenterLog.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Azure.Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CallCenterApp.Models
+{
+    public class CallCenterLog
+    {
+        public float searchscore { get; set; }
+        public string id { get; set; }
+        public string audio_file { get; set; }
+        public string metadata_storage_content_type { get; set; }
+        public int metadata_storage_size { get; set; }
+        public DateTimeOffset? metadata_storage_last_modified { get; set; }
+        public string metadata_storage_name { get; set; }
+        public string metadata_storage_path { get; set; }
+        public string transcription_job { get; set; }
+        public string language { get; set; }
+        public double audio_length_in_seconds { get; set; }
+        public object channel0 { get; set; }
+        public object channel1 { get; set; }
+        public Summary summary { get; set; }
+        public Conversation[] conversation { get; set; }
+    }
+
+    public class Summary
+    {
+        public float max_change { get; set; }
+        public int max_change_index { get; set; }
+        public float max_change_time { get; set; }
+        public float lowest_sentiment { get; set; }
+        public float highest_sentiment { get; set; }
+    }
+
+    public class Conversation
+    {
+        public int? turn { get; set; }
+        public string speaker { get; set; }
+        public string text { get; set; }
+        public long? offset { get; set; }
+        public int? duration { get; set; }
+        public float? offset_in_seconds { get; set; }
+        public float? duration_in_seconds { get; set; }
+        public float? sentiment { get; set; }
+        public string[] locations { get; set; }
+        public string[] organizations { get; set; }
+        public string[] people { get; set; }
+        public string[] key_phrases { get; set; }
+        public DateTimeOffset? creation_date { get; set; }
+    }
+}
+
